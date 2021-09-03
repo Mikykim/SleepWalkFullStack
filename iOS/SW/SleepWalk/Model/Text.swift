74b4ba5c70e75fe55bba6d830bacbd6ec1e30e77
Kevin <56335026+kevinumarov@users.noreply.github.com>
2021-09-03 18:51:41 +0900

    Add files via upload

---
+++ iOS/SW/SleepWalk/Model/Text.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  SW
+// sections:
+//Onboarding
+//Core
+//Sidemenus
+//Others
+
+import Foundation
+struct Text {
+    static let sw = "SleepWalk"
+    static let ok = "확인"
+    static let cancel = "취소"
+    // Onboarding Login section
+    static let login = "로그인"
+    static let id = "아이디"
+    static let password = "비밀번호"
+    static let goreg = "회원가입"
+    // Onboarding register section
+    static let register = "REGISTER"
+    static let name = "이름"
+    static let phoneNumber = "전화번호"
+    static let vercode = "인증번호"
+    static let regComplete = "회원가입 완료"
+    // Core View Controllers
+    static let welcome = "반갑습니다!!! %@님"
+    static let order = "Order"
+    static let stamp = "Stamp"
+    static let event = "Event"
+    static let receipt = "Receipt"
+    // Sidemenu Section
+    static let userName = "%@님"
+    static let menu = "메뉴"
+    static let orderSide = "오더"
+    static let eventSide = "이벤트"
+    static let receiptSide = "영수증"
+    static let changeInfo = "정보수정"
+    static let motto = "NEVER STOP DREAMING"
+    
+    
+}
